0b08d164623497b8842a5a897e8ffdc5b05fdb1d
abnaim33 <67532457+abnaim33@users.noreply.github.com>
2021-03-07 17:43:58 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import { ChromePicker } from 'react-color';
 import './App.css';
 
 function App() {
+  const [color, setColor] = useState('#fff')
+  const [showColorPicker, setShowColorPicker] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <button className="color_button"
+        onClick={() => setShowColorPicker(showColorPicker => !showColorPicker)}>
+        {showColorPicker ? 'Close color picker' : 'pick a color'}
+      </button>
+
+      {showColorPicker && (
+
+        <ChromePicker color={color} onChange={updatedColor => setColor(updatedColor.hex)}></ChromePicker>
+
+      )}
+      <h1>your color is {color}</h1>
+
+      {/* finish everyting,just have to likedup to my portfolio */}
+
+
+      <h2>Created by <a className="portfolio_link" href="#">Naim Hossen</a></h2>
     </div>
   );
 }
